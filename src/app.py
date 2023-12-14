62fbea6574e9ba21a7329d70227c87f0ea752cca
Renan Santana <renan.santana@instruct.com.br>
2023-12-14 17:33:43 -0300

    Fix style

--- src/app.py
+++ src/app.py
@@ -1,14 +1,12 @@
-import configparser
 import uuid
 import os
-from flask import Flask, render_template, request, url_for, redirect, jsonify
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 
 DATABASE_URL = os.getenv('DATABASE_URL')
-
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 
@@ -29,6 +27,9 @@ with app.app_context():
     db.create_all()
 
 
+def generateKey():
+    return uuid.uuid4()
+
 @app.route("/")
 @app.route("/home")
 def index():
@@ -36,22 +37,14 @@ def index():
 
 @app.route("/saveContent", methods=["POST"])
 def saveContent():
-        content = request.form.get('content')
-        key = str(generateKey())
-        paste = Pastes(key, content)
-        db.session.add(paste)
-        db.session.commit()
-        return jsonify({'sucess' : True, 'key' : key})
+    content = request.form.get('content')
+    key = str(generateKey())
+    paste = Pastes(key, content)
+    db.session.add(paste)
+    db.session.commit()
+    return jsonify({'sucess' : True, 'key' : key})
 
 @app.route("/<key>")
 def getPaste(key):
     paste = Pastes.query.filter_by(key=key).first()
     return render_template("editor.html", content=paste.content)
-
-def generateKey():
-    return uuid.uuid4()
-
-if __name__ == '__main__':
-    # app.run(debug=True)
-    port = os.getenv('PORT', 5000)
-    app.run(host='0.0.0.0', port=port)
